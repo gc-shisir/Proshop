793a1b7c78880a748e466a86e5329848a8917fcf
Shisir GC <shisir.gc44@gmail.com>
2021-07-22 16:41:16 +0545

    completed database connection and redux implementation for product part

---
+++ backend/routes/productRoutes.js
@@ -0,0 +1,41 @@
+import express from "express";
+
+import asyncHandler from "express-async-handler";
+import Product from "../models/productModel.js";
+
+const router = express.Router();
+
+// @desc Fetch all products
+// @route GET /api/products
+// @access Public
+router.get(
+  "/",
+  asyncHandler(async (req, res) => {
+    const products = await Product.find({});
+    res.json(products);
+  })
+);
+
+// @desc Fetch single products
+// @route GET /api/products/:id
+// @access Public
+router.get(
+  "/:id",
+  asyncHandler(async (req, res) => {
+    const product = await Product.findById(req.params.id);
+    // .then((doc) => res.json(doc))
+    // .catch((err) =>
+    //   res.status(404).json({ msg: "Product Not Found", error: err.message })
+    // );
+
+    // if used async handler,remove then()..catch() block and execute code below.
+    if (product) {
+      res.json(product);
+    } else {
+      res.status(404);
+      throw new Error("Product Not Found");
+    }
+  })
+);
+
+export default router;
--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,28 @@
 import express from "express";
 import dotenv from "dotenv";
 
-import products from "./data/products.js";
+import colors from "colors";
+import connectDB from "./config/db.js";
+import productRoutes from "./routes/productRoutes.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
 dotenv.config();
+
+connectDB();
 
 const app = express();
 
 app.get("/", (req, res) => {
   res.send("API is running");
 });
-app.get("/api/products", (req, res) => {
-  res.json(products);
-});
-app.get("/api/products/:id", (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
-});
+
+app.use("/api/products", productRoutes);
+
+// not found route
+app.use(notFound);
+
+// custom error handling middleware
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
@@ -24,5 +30,6 @@ app.listen(
   PORT,
   console.log(
     `server running in ${process.env.NODE_ENV} mode on port ${process.env.PORT}`
+      .yellow.bold
   )
 );
