dd54f6bb8a096fff680f21d3d10fa3a726a2fc70
Shisir GC <shisir.gc44@gmail.com>
2021-07-30 01:19:33 +0545

    added user registration and password encryption code

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -15,6 +15,17 @@ userSchema.methods.matchPassword = async function (enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password);
 };
 
+userSchema.pre("save", async function (next) {
+  // to check if password is newly added or modified i.e. only hash password if user is new or the 
+  // password is updated. otherwise, new hash will be generated and we cannot decrypt it. 
+  if (!this.isModified("password")) {
+    next;
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
 const User = mongoose.model("User", userSchema);
 
 export default User;
